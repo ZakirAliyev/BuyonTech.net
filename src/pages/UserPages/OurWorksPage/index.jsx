822fea5a32def84e2c051143ea48f2bdb5fed467
Пользователь <tu7c1ne72@code.edu.az>
2025-10-14 15:03:40 +0400

    add

--- src/pages/UserPages/OurWorksPage/index.jsx
+++ src/pages/UserPages/OurWorksPage/index.jsx
@@ -1,11 +1,27 @@
 import './index.scss'
 import Navbar from "../../../components/UserComponents/Navbar/index.jsx";
-import {IoChevronDownOutline} from "react-icons/io5";
+import { IoChevronDownOutline } from "react-icons/io5";
 import WorkCard from "../../../components/UserComponents/WorkCard/index.jsx";
 import mid from "/src/assets/mid.png";
 import nas from "/src/assets/nas.png";
+import { useEffect, useState } from 'react';
+import Footer from '../../../components/UserComponents/Footer/index.jsx';
 
 function OurWorksPage() {
+    const [footerHeight, setFooterHeight] = useState(0);
+    const [showFooter, setShowFooter] = useState(false);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setShowFooter(true), 0);
+        return () => clearTimeout(timer);
+    }, []);
+
+    useEffect(() => {
+        const handleResize = () => setWindowWidth(window.innerWidth);
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
     const worksData = [
         {
@@ -82,26 +98,52 @@ function OurWorksPage() {
         }
     ];
     return (
-        <section id={"ourWorksPage"}>
-            <Navbar/>
-            <div className={"container"}>
-                <div className={"ourWorks"}>
-                    <div className={"textWrapper"}>
-                        <h2>Our Works</h2>
-                        <div className={"select"}>
-                            <div className={"option"}>
-                                All projects
-                                <IoChevronDownOutline className={"icon"}/>
+
+
+        <section id={"ourWorksPage"} style={{ overflow: "hidden", position: "relative", background: "#fff" }}>
+
+            <div style={{
+                position: "relative",
+                zIndex: 100,
+                background: "var(--bg-color)",
+            }}>
+                <Navbar />
+                <div className={"container"}>
+                    <div className={"ourWorks"} style={{ paddingBottom: "16px"}}>
+                        <div className={"textWrapper"}>
+                            <h2>Our Works</h2>
+                            <div className={"select"}>
+                                <div className={"option"}>
+                                    All projects
+                                    <IoChevronDownOutline className={"icon"} />
+                                </div>
                             </div>
                         </div>
+                        {worksData.map((work) => (
+                            <WorkCard key={work.id} {...work} />
+                        ))}
                     </div>
-                    {worksData.map((work) => (
-                        <WorkCard key={work.id} {...work} />
-                    ))}
                 </div>
+
             </div>
+            {showFooter && (
+                windowWidth >= 992 ? (
+                    <>
+                        <div style={{
+                            height: `${footerHeight}px`,
+                            position: "relative",
+                            zIndex: -90,
+                        }}></div>
+                        <Footer setFooterHeight={setFooterHeight} />
+                    </>
+                ) : (
+                    <Footer setFooterHeight={setFooterHeight} />
+                )
+            )}
         </section>
     );
 }
 
 export default OurWorksPage;
+
+
