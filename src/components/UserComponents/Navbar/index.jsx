875ce47a950601cd77e31ef5beb0189c72520c97
Пользователь <tu7c1ne72@code.edu.az>
2025-10-16 16:38:58 +0400

    add hamburger

--- src/components/UserComponents/Navbar/index.jsx
+++ src/components/UserComponents/Navbar/index.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import './index.scss'
 import { useTranslation } from "react-i18next";
 import { TfiWorld } from "react-icons/tfi";
 import logo from "/src/assets/sariLogo.webp"
 import { useNavigate } from "react-router";
 import { FaBarsStaggered } from "react-icons/fa6";
+import MobileMenu from '../MobileMenu';
 
 function Navbar() {
+    const [open, setOpen] = useState(false);
+
 
     const { t } = useTranslation();
     const navigate = useNavigate();
@@ -17,34 +21,50 @@ function Navbar() {
         });
     }
     return (
-        <section id={"navbar"}>
-            <div className={"container"}>
-                <nav>
-                    <div className={"links"}>
-                        <div onClick={() => {
-                            handleClickLink('/')
-                        }} className={"link"}>Home</div>
-                        <div onClick={() => {
-                            handleClickLink('/about')
-                        }} className={"link"}>About</div>
-                        <div onClick={() => {
-                            handleClickLink('/our-works')
-                        }} className={"link"}>Portfolio</div>
-                        <div onClick={() => {
-                            handleClickLink('/')
-                        }} className={"link"}>FAQ</div>
+        <>
+            <section id={"navbar"}>
+                <div className={"container"}>
+                    <nav>
+                        <div className={"links"}>
+                            <div onClick={() => {
+                                handleClickLink('/')
+                            }} className={"link"}>Home</div>
+                            <div onClick={() => {
+                                handleClickLink('/about')
+                            }} className={"link"}>About</div>
+                            <div onClick={() => {
+                                handleClickLink('/our-works')
+                            }} className={"link"}>Portfolio</div>
+                            <div onClick={() => {
+                                handleClickLink('/')
+                            }} className={"link"}>FAQ</div>
+                        </div>
+                        <img src={`${logo}`} alt={"Logo"} onClick={() => navigate("/")} />
+                        <div className={"buttonWrapper"}>
+                            <button onClick={() => {
+                                handleClickLink('/contact')
+                            }}>Contact us</button>
+                            <TfiWorld className={"icon"} style={{ fontSize: "20px" }} />
+                        </div>
+                        <MobileMenu setOpen={setOpen} open={open} />
+                    </nav>
+                </div>
+            </section>
+
+
+            {open && (
+                <div className="menuOverlay">
+                    <div className="menuBox">
+                        <nav className="menuLinks">
+                            <div onClick={() => handleClickLink("/")}>Home</div>
+                            <div onClick={() => handleClickLink("/about")}>About</div>
+                            <div onClick={() => handleClickLink("/our-works")}>Portfolio</div>
+                            <div onClick={() => handleClickLink("/contact")}>Contact</div>
+                        </nav>
                     </div>
-                    <img src={`${logo}`} alt={"Logo"} onClick={() => navigate("/")} />
-                    <div className={"buttonWrapper"}>
-                        <button onClick={() => {
-                            handleClickLink('/contact')
-                        }}>Contact us</button>
-                        <TfiWorld className={"icon"} style={{ fontSize: "20px" }} />
-                    </div>
-                    <FaBarsStaggered className={"menuBar"} />
-                </nav>
-            </div>
-        </section>
+                </div>
+            )}
+        </>
     );
 }
 
